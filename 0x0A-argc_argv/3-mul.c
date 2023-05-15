386f5cfe570fea2d2705e06bef28f03936cfdbab
Eriny <erinyantonious@gmail.com>
2023-05-15 08:53:42 -0700

    fourth task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - program that multiplies two numbers.
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int product;
+
+	if (argc == 3)
+	{
+		product = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", product);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
